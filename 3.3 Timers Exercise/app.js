b7b61fac29f1efbd141272b16fcdf22ff4e98080
andy9299 <andy.tu998@gmail.com>
2022-06-23 12:40:51 -0400

    initial commit

---
+++ 3.3 Timers Exercise/app.js
@@ -0,0 +1,24 @@
+function countDown(num){
+    let interval = setInterval(function(counter){
+        num--;
+        if (!num) {
+            console.log("DONE!");
+            clearInterval(interval);
+        }
+        else {
+            console.log(num);
+        }
+    }, 1000);
+}
+
+function randomGame(){
+    let counter = 0;
+    let interval = setInterval(
+        function(){
+            counter++;
+            if (Math.random() > .75){
+                clearInterval(interval);
+                console.log(counter);
+            }
+        }, 1000);
+}
