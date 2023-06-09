a10494111028705fd3cd9b06e124b907834f8015
andy9299 <andy.tu998@gmail.com>
2023-06-09 16:15:19 -0400

    exercise 39.9 part 2

--- Exercises/Chapter 39/39.9 React Effects And Refs/src/Deck.js
+++ Exercises/Chapter 39/39.9 React Effects And Refs/src/Deck.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import axios from "axios";
 import Card from "./Card";
 
@@ -7,6 +7,8 @@ const BASE_URL = "http://deckofcardsapi.com/api/deck";
 function Deck() {
   const [deck, setDeck] = useState(null);
   const [drawnCards, setDrawnCards] = useState([]);
+  const [autoDraw, setAutoDraw] = useState(false);
+  const timerId = useRef(null);
 
   useEffect(() => {
     async function getData() {
@@ -21,32 +23,47 @@ function Deck() {
     getData();
   }, []);
 
-  const draw = () => {
-    async function getCard() {
-      try {
-        let res = await axios.get(`${BASE_URL}/${deck.deck_id}/draw`);
-        if (!res.data.success && res.data.remaining === 0) throw new Error("No more cards to draw!");
-        let card = res.data.cards[0];
-        setDrawnCards([...drawnCards, {
-          id: card.code,
-          name: `${card.value} ${card.suit}`,
-          image: card.image
-        }]);
+  async function draw() {
+    try {
+      const res = await axios.get(`${BASE_URL}/${deck.deck_id}/draw`);
+      if (!res.data.success && res.data.remaining === 0) {
+        setAutoDraw(false);
+        throw new Error("No more cards to draw!");
       }
-      catch (err) {
-        alert(err);
-      }
+      const card = res.data.cards[0];
+      setDrawnCards(d => [...d, {
+        id: card.code,
+        name: `${card.value} ${card.suit}`,
+        image: card.image
+      }]);
     }
-    getCard();
+    catch (err) {
+      alert(err);
+    }
+  }
+
+  useEffect(() => {
+    if (autoDraw && !timerId.current) {
+      timerId.current = setInterval(async () => {
+        await draw();
+      }, 1000);
+    }
+    return () => {
+      clearInterval(timerId.current);
+      timerId.current = null;
+    };
+
+  }, [autoDraw, deck]);
+
+  const toggleAutoDraw = () => {
+    setAutoDraw(auto => !auto);
   };
 
   const cards = drawnCards.map(card => <Card key={card.id} name={card.name} image={card.image} />);
 
   return (
-
     <div>
-
-      <button onClick={draw}>Draw A Card</button>
+      <button onClick={toggleAutoDraw}>Toggle Auto Draw</button>
       <div>
         {cards}
       </div>
