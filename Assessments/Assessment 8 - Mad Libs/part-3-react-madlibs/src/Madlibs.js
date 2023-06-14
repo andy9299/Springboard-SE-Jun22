55c9c82e75939d38fbb1521789c6b509672c187d
andy9299 <andy.tu998@gmail.com>
2023-06-14 17:04:22 -0400

    story displayed properly

--- Assessments/Assessment 8 - Mad Libs/part-3-react-madlibs/src/Madlibs.js
+++ Assessments/Assessment 8 - Mad Libs/part-3-react-madlibs/src/Madlibs.js
@@ -3,14 +3,17 @@ import Story from './Story';
 import emptyMadlibs from './emptyMadlibs';
 
 function Madlibs() {
-  const [madlib, setMadlib] = useState({
-    ...emptyMadlibs[0], words: {
-      "noun 1": "slug",
-      "noun 2": "butterfly",
-      "adjective": "hungry",
-      "color": "purple"
-    }
-  });
+  const [madlib, setMadlib] = useState(emptyMadlibs[0]);
+  const updateWords = (wordsObj) => {
+    setMadlib({ ...madlib, words: wordsObj });
+  };
+  useEffect(() => updateWords({
+    "noun 1": "slug",
+    "noun 2": "butterfly",
+    "adjective": "hungry",
+    "color": "purple"
+  }
+  ), []);
   return (
     <div>
       <Story madlib={madlib} />
---
+++ Assessments/Assessment 8 - Mad Libs/part-3-react-madlibs/src/MadlibsForm.js
@@ -0,0 +1,7 @@
+import React, { useState } from "react";
+import { v4 as uuid } from 'uuid';
+
+function MadlibsForm() {
+
+}
+export default MadlibsForm;
