bf4ba62e26c66672ea8bb54ab16a337b378c93a9
andy9299 <andy.tu998@gmail.com>
2023-07-26 20:53:03 -0400

    47.3 exercise

---
+++ Exercises/Chapter 47/47.3 Divide and Conquer/sorted-frequency.js
@@ -0,0 +1,35 @@
+function sortedFrequency(arr, num) {
+  let firstIdx = findFirstNum(arr, num);
+  if (firstIdx == -1) return firstIdx;
+  let lastIdx = findLastNum(arr, num);
+  console.log(firstIdx, lastIdx);
+  return lastIdx - firstIdx + 1;
+}
+
+function findFirstNum(arr, num, low = 0, high = arr.length - 1) {
+  if (high >= low) {
+    let mid = Math.floor((high + low) / 2);
+    if ((mid === 0 || num > arr[mid - 1]) && arr[mid] === num) {
+      return mid;
+    } else if (num > arr[mid]) {
+      return findFirstNum(arr, num, mid + 1, high);
+    }
+    return findFirstNum(arr, num, low, mid - 1);
+  }
+  return -1;
+}
+
+function findLastNum(arr, num, low = 0, high = arr.length - 1) {
+  if (high >= low) {
+    let mid = Math.floor((high + low) / 2);
+    if ((mid === arr.length - 1 || num < arr[mid + 1]) && arr[mid] === num) {
+      return mid;
+    } else if (num < arr[mid]) {
+      return findLastNum(arr, num, low, mid - 1);
+    }
+    return findLastNum(arr, num, mid + 1, high);
+  }
+  return -1;
+}
+
+module.exports = sortedFrequency;
